e26de4bb81e00d4a9e81954911891f744a8670b5
kanda <j1613@matsue-ct.jp>
2019-10-02 12:15:05 +0900

    modified by kanda

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,11 @@
 for i in 1..100 
     if i % 3 == 0 && i % 5 == 0
       print("FizzBuzz\n")
-     elsif i % 3 == 0)
+     elsif i % 3 == 0
       print("Fizz\n")
      elsif i % 5 == 0 
       print("Buzz\n")
     else 
-      print(i,"n")
-    }
-  }
-
-  return 0;
-}
-
+      print(i,"\n")
+    end
+end 
